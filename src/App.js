20166b759ebe25d54aacb4537171b015987a8013
mohammed haitgham <modyhamody1999@gmail.com>
2023-04-12 15:54:29 +0300

    in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './TodoList.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+    const [newTodoText, setNewTodoText] = useState('');
+
+    function handleNewTodoTextChange(event) {
+        setNewTodoText(event.target.value);
+    }
+
+    function handleAddTodo() {
+        if (newTodoText !== '') {
+            setTodos([...todos, { text: newTodoText, completed: false }]);
+            setNewTodoText('');
+        }
+    }
+
+    function handleDeleteTodo(index) {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    }
+
+    function handleCompleteToggle(index) {
+        const newTodos = [...todos];
+        newTodos[index].completed = !newTodos[index].completed;
+        setTodos(newTodos);
+    }
+
+    return ( <
+        div className = "TodoList" >
+        <
+        h1 > To - Do List < /h1> <
+        ul > {
+            todos.map((todo, index) => ( <
+                li key = { index } >
+                <
+                label className = { todo.completed ? 'completed' : null } >
+                <
+                input type = "checkbox"
+                checked = { todo.completed }
+                onChange = {
+                    () => handleCompleteToggle(index) }
+                /> { todo.text } <
+                /label> <
+                button onClick = {
+                    () => handleDeleteTodo(index) } > Delete < /button> <
+                /li>
+            ))
+        } <
+        /ul> <
+        input type = "text"
+        value = { newTodoText }
+        onChange = { handleNewTodoTextChange }
+        /> <
+        button onClick = { handleAddTodo } > Add < /button> <
+        /div>
+    );
 }
 
-export default App;
+export default TodoList;
